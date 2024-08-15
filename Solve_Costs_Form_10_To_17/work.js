576d3d8b82f84486eb671cae3e8e8f2d441f63cc
Mrit Asid <ww771282337@gmail.com>
2024-08-15 03:22:34 +0300

    تمت اضافة حل التكاليف من 10 الى 17

---
+++ Solve_Costs_Form_10_To_17/work.js
@@ -0,0 +1,52 @@
+                    // =====================
+                    // =          1        =
+                    // =====================
+
+/* 
+                    let one = 10;
+let two = 20;
+
+console.log(one + "" + two)// Normal Concatenate => 1020
+console.log(typeof (one + "" + two)) // Normal Concatenate => String
+
+console.log(`${one}${two}`); // Template Literals Way => 1020
+console.log(typeof `${one}${two}`); // Template Literals Way => String
+
+
+console.log(two + "\n" + one);
+console.log(`${two}
+${one}`); 
+
+ */
+
+                    // =====================
+                    // =          2        =
+                    // =====================
+
+ /*                    
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+                                     //    work2.html  <==  الحل في ملف 
+
+ */
+                    // =====================
+                    // =          3        =
+                    // =====================
+/* 
+console.log(" \`I\'m In\n \\\\ \n Love \\\\ \"\"\" ''' \n ++ With ++ \n \\\"\"\"\\\"\"\" " +
+"\n \"\"JavaScript\"\"`` ");
+                        
+   */                      
+
+
+                    // =====================
+                    // =          4        =
+                    // =====================
+
+/* 
+let a = 21;
+let b = 20;
+
+let markUp = `_${a}${b}`;
+console.log(`_${a}${markUp.repeat(3)}_${b}_`)
+ */
